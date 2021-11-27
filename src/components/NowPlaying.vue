28cd69dcc97697acf946d7cee2631156e1478458
Kumar Challa <chvvkumar@gmail.com>
2021-11-26 22:36:31 -0600

    remove text div

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -12,10 +12,6 @@
           class="now-playing__image"
         />
       </div>
-      <div class="now-playing__details">
-        <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
-        <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
-      </div>
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
       <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
