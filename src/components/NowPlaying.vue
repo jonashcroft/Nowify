7d28c900f29946ba463cafe588967af75e65832b
KSuondProject <70426429+KSuondProject@users.noreply.github.com>
2022-08-24 13:40:46 +0200

    Traduzione testo per "nessuna traccia in riproduzione!

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -18,7 +18,7 @@
       </div>
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
-      <h1 class="now-playing__idle-heading">No music is playing 😔</h1>
+      <h1 class="now-playing__idle-heading">Spotify in attesa di pezzoni 😎</h1>
     </div>
   </div>
 </template>
