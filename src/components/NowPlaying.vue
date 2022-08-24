a5f23cdb333aa76f688562052642b60d0d694399
KSuondProject <70426429+KSuondProject@users.noreply.github.com>
2022-08-24 15:46:29 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -15,7 +15,7 @@
       <div class="now-playing__details">
         <h1 class="now-playing__track" v-text="player.trackTitle"></h1>
         <h2 class="now-playing__artists" v-text="getTrackArtists"></h2>
-        <h2 class="now-playing__album" v-text="getTrackAlbum"></h2>
+        <h2 class="now-playing__album" v-text="getAlbumYear"></h2>
       </div>
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
