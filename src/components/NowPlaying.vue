2f12db1e9e38f82775027bdc0197ce419270cb07
KSuondProject <70426429+KSuondProject@users.noreply.github.com>
2022-08-25 08:12:29 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -234,7 +234,7 @@ export default {
         ),
         trackTitle: this.playerResponse.item.name,
         trackId: this.playerResponse.item.id,
-        trackYear: this.playerResponse.item.album.release_date,
+        trackYear: this.playerResponse.item.album.release_date[0],
         trackAlbumName: this.playerResponse.item.album.name,
         trackAlbum: {
           title: this.playerResponse.item.album.name,
