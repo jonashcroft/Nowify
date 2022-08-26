ce3d2ee448be4132cbd1d9cd917ede351d85beb3
KSuondProject <70426429+KSuondProject@users.noreply.github.com>
2022-08-26 13:40:42 +0200

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -167,7 +167,7 @@ export default {
         trackTitle: '',
         trackYear: '',
         trackAlbumName: '',
-        TrackTempo: ''
+        trackTempo: ''
       }
     },
 
@@ -236,7 +236,7 @@ export default {
         ),
         trackTitle: this.playerResponse.item.name,
         trackId: this.playerResponse.item.id,
-        TrackTempo: this.playerResponse.track.tempo.toString(),
+        trackTempo: this.playerResponse.track.tempo.toString(),
         trackYear: this.playerResponse.item.album.release_date.substring(0,4),
         trackAlbumName: this.playerResponse.item.album.name,
         trackAlbum: {
