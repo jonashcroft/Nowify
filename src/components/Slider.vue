413530dd68608f4bea38098229b54a2ca31ea646
sleodc <50521983+sleodc@users.noreply.github.com>
2024-02-08 11:55:18 +0800

    Update Slider.vue

--- src/components/Slider.vue
+++ src/components/Slider.vue
@@ -1,57 +1,7 @@
 <template>
-  <div class="carousel-container">
-    <div class="carousel" ref="carousel">
-      <img v-for="(image, index) in images" :key="index" :alt="'Image ' + (index + 1)" class="carousel-slide">
+    <div class="slide">
+        <transition name="slide">
+            <slot />
+        </transition>
     </div>
-  </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      currentIndex: 0,
-      images: [
-        require('../../assets/preview-1.png') ,
-        require('../../assets/preview-2.png'),
-        require('../../assets/preview-3.png') 
-      ],
-      autoplayInterval: null
-    };
-  },
-  mounted() {
-    this.startAutoplay();
-  },
-  beforeDestroy() {
-    this.stopAutoplay();
-  },
-  methods: {
-    startAutoplay() {
-       this.autoplayInterval = setInterval(this.nextSlide.bind(this), 3000);
-    },
-    stopAutoplay() {
-      clearInterval(this.autoplayInterval);
-    },
-    nextSlide() {
-      this.currentIndex = (this.currentIndex + 1) % this.images.length;
-    }
-  }
-};
-</script>
-
-<style>
-.carousel-container {
-  width: 100%;
-  overflow: hidden;
-}
-
-.carousel {
-  display: flex;
-  transition: transform 0.5s ease;
-}
-
-.carousel-slide {
-  width: 100%;
-  flex: 0 0 auto;
-}
-</style>
