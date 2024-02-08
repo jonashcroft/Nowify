424a341812a155417790b001ac10e25eaff311e6
sleodc <50521983+sleodc@users.noreply.github.com>
2024-02-08 11:57:34 +0800

    Update NowPlaying.vue

--- src/components/NowPlaying.vue
+++ src/components/NowPlaying.vue
@@ -19,9 +19,9 @@
     </div>
     <div v-else class="now-playing" :class="getNowPlayingClass()">
       <Carousel class="carousel" v-slot="{ currentSlide }">
-      <Slide v-for="(slide, index) in carouselSlides" :key="index">
-        <div v-show="currentSlide == index + 1" class="slide-info">
-          <img :src="require(`../assets/${slide}.png`)" alt="">
+        <Slide v-for="(slide, index) in carouselSlides" :key="index">
+          <div v-show="currentSlide == index + 1" class="slide-info">
+            <img :src="require(`../assets/${slide}.png`)" alt="">
         </div>
       </Slide>
     </Carousel>
