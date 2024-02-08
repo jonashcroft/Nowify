692bb746b6fe5dc68134d170dbabe8fb14429107
sleodc <50521983+sleodc@users.noreply.github.com>
2024-02-08 12:52:15 +0800

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/no-unused-components": "off"
+    "vue/no-unused-components": "off",
+    'vue/no-unused-vars': 'off'
   }
 };
