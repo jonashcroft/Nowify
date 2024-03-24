709c2fcfad7e82ae9b6b40946acfe163705e6734
Menno Liefstingh <git@menno.ai>
2024-03-24 02:05:54 +0100

    fix: nothing to see here

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-import { createApp } from 'vue'; // Update import statement for Vue 3
+import { createApp } from 'vue';
 import App from './App.vue';
 
 import '@/styles/global/reset.scss';
 import '@/styles/global/app.scss';
 
-// Gebruik createApp in plaats van new Vue voor Vue 3
 const app = createApp(App);
-app.mount('#app'); // Vervangt .$mount('#app')
+app.mount('#app');
